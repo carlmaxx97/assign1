b5c68401df554dfcd90f2972003811204139a5a8
carlmaxx97 <47651872+carlmaxx97@users.noreply.github.com>
2019-10-27 15:36:12 +0800

    Add files via upload

--- script/enhancements.js
+++ script/enhancements.js
@@ -1,25 +1,3 @@
-function a()
-{
-	
-	// Get the modal
-	var modal = document.getElementById("myModal");
-
-	// Get the image and insert it inside the modal
-	var img = document.getElementById("myImg");
-	var modalImg = document.getElementById("img01");
-
-	modal.style.display = "block";
-	modalImg.src = img.src;
-	
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() { 
-	  modal.style.display = "none";
-	}
-}
-
 /*set the navigation tab or item highlighted if the stay on proper page*/
 function navigation_highlight()
 {
@@ -54,29 +32,23 @@ function form_submission()
 	}
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
+// Next/previous controls button of the slideshow
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+//Display the image of the slideshow
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
