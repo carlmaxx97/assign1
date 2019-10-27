91def6f6854a3dcef19a7a5de2d396c9f8a40b24
carlmaxx97 <47651872+carlmaxx97@users.noreply.github.com>
2019-10-27 16:17:13 +0800

    Update enhancements.js

--- script/enhancements.js
+++ script/enhancements.js
@@ -46,5 +46,5 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides(slideIndex), 2000); // Change image every 2 seconds
 }
